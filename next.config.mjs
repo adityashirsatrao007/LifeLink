cb47ec393c2c99b17dcfbfed672c900fd9251250
Aditya <adityashirsatrao007@gmail.com>
2025-11-06 00:33:16 +0530

    Add environment variables to Docker build and Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_PARSE_APP_ID: process.env.NEXT_PUBLIC_PARSE_APP_ID,
+    NEXT_PUBLIC_PARSE_JS_KEY: process.env.NEXT_PUBLIC_PARSE_JS_KEY,
+    NEXT_PUBLIC_PARSE_SERVER_URL: process.env.NEXT_PUBLIC_PARSE_SERVER_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+  },
 };
 
 export default nextConfig;
